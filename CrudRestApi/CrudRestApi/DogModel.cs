2273b822cc54246db652f687b87beb4b3b4b9a1e
Enrico Sabbadin <enrico.sabbadin@msc.com>
2024-10-17 16:10:58 +0200

    first commit

---
+++ CrudRestApi/CrudRestApi/DogModel.cs
@@ -0,0 +1,17 @@
+﻿namespace CrudRestApi
+{
+    public class DogModel : DogModelUpdate
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+
+    }
+
+    public class DogModelUpdate
+    {
+     
+        public string Name { get; init; } = "";
+
+        public int Age { get; init; }
+
+    }
+}
---
+++ CrudRestApi/readme.txt
@@ -0,0 +1,4 @@
+Requires .NET 8.0
+Run the project and use the swagger ui the test the api. 
+or use the postman collection 'CrudRestApi.postman_collection.json' to test the api.	
+you can downlaod postman here: https://www.postman.com/
